deb771286340ba530be210024f686b73f0f56256
= <optikcrash@hotmail.com>
2022-02-04 16:10:27 -0800

    init

---
+++ lib/widgets/n_buttons.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NButton extends StatefulWidget {
+  const NButton({Key? key}) : super(key: key);
+
+  @override
+  _NButtonState createState() => _NButtonState();
+}
+
+class _NButtonState extends State<NButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+var temp = ButtonStyleButton();
