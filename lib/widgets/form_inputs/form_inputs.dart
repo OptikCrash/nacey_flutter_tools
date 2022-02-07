c415e4d41b713d52ab1f0255071ec5916ab9ac18
= <optikcrash@hotmail.com>
2022-02-06 21:41:31 -0800

    search

--- lib/widgets/form_inputs/form_inputs.dart
+++ lib/widgets/form_inputs/form_inputs.dart
@@ -1,3 +1,3 @@
 export 'n_text_field.dart' show NTextField;
 export 'n_number_field.dart' show NNumericField;
-export 'n_search.dart' show NSearchField, NSearchDelegate;
+export 'n_search.dart' show NSearchField, NSearchDelegate, showNSearchDelegate;
